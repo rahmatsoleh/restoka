16dc4027630193a2bfa52ccc9d5ffe956c5ccc18
Rahmat Soleh <solehr619@gmail.com>
2022-03-17 17:28:42 +0700

    Creating Slogan Component

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import './views/container/header.js';
 import './views/container/hero.js';
 import './views/container/catagory-food.js';
+import './views/container/slogan-comp.js';
 
 
 document.querySelector('header').innerHTML = `<header-element></header-element>`;
@@ -11,6 +12,8 @@ document.querySelector('header').innerHTML = `<header-element></header-element>`
 const main = `
     <hero-element></hero-element>
     <category-food></category-food>
+    <slogan-comp></slogan-comp>
+    <div style="min-height:500px">lorem</div>
 `;
 
 document.querySelector('main').innerHTML = main;
---
+++ src/scripts/views/container/slogan-comp.js
@@ -0,0 +1,22 @@
+import '../../../styles/component/slogan-comp.scss';
+
+class SloganComp extends HTMLElement {
+    connectedCallback(){
+        this.render();
+    }
+
+    render(){
+        this.innerHTML = `
+            <article id="slogan-comp">
+                <div class="thumb-image">
+                    <img src="images/heros/hero-image_1.jpg" alt="">
+                </div>
+                <div class="slogan-desc">
+                    <p>There are more than 1000 restaurants for those of you who want to find food</p>                
+                </div>
+            </article>
+        `;
+    }
+}
+
+customElements.define('slogan-comp', SloganComp);
